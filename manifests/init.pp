f76dd92af4236420c1ce444fb0a79f03b1a16e6e
Chris Webster <chirs.webster@puppet.com>
2019-12-31 09:59:31 -0800

    Added main class for module

---
+++ manifests/init.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include puppet_logging_dashboard
+class puppet_logging_dashboard {
+}
