eb0c61367e6277ec5d89a1b87036d0f92239b5fe
Chris Webster <chirs.webster@puppet.com>
2020-06-29 14:36:16 -0700

    asdf

--- manifests/install_elastic.pp
+++ manifests/install_elastic.pp
@@ -7,29 +7,29 @@
 class puppet_logging_dashboard::install_elastic {
   include elastic_stack::repo
   notify { 'Installing Elastic Search':
-    message => 'Installing Elastic Search'
+    message => 'Installing Elastic Search',
   }
 
   elasticsearch::instance { 'es-01':
-  #   jvm_options => [
-  #     '-Xms4g',
-  #     '-Xmx4g',
-  #     '#PrintGCDetails',
-  #     '#PrintGCDateStamps',
-  #     '#PrintTenuringDistribution',
-  #     '#PrintGCApplicationStoppedTime',
-  #     '#Xloggc',
-  #     '#UseGCLogFileRotation',
-  #     '#NumberOfGCLogFiles',
-  #     '#GCLogFileSize',
-  #     '#XX:UseConcMarkSweepGC',
-  #   ],
-  #   config      => {
-  #     #  'xpack.monitoring.collection.enabled' => true,
-  #     'network.host'                        => '0.0.0.0',
-  #     'http.port'                           => '9200',
-  #     'cluster.initial_master_nodes'        => 'elastic.puppetdebug.vlan',
-  #     'xpack.monitoring.collection.enabled' =>  true,
-  #   },
+    jvm_options => [
+      '-Xms4g',
+      '-Xmx4g',
+      '#PrintGCDetails',
+      '#PrintGCDateStamps',
+      '#PrintTenuringDistribution',
+      '#PrintGCApplicationStoppedTime',
+      '#Xloggc',
+      '#UseGCLogFileRotation',
+      '#NumberOfGCLogFiles',
+      '#GCLogFileSize',
+      '#XX:UseConcMarkSweepGC',
+    ],
+    config      => {
+      #  'xpack.monitoring.collection.enabled' => true,
+      'network.host'                        => '0.0.0.0',
+      'http.port'                           => '9200',
+      'cluster.initial_master_nodes'        => 'elastic.puppetdebug.vlan',
+      'xpack.monitoring.collection.enabled' =>  true,
+    },
   }
 }
