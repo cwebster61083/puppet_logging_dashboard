d532e49707d6ae39a996f4eaba64eae4f195f815
Chris Webster <chirs.webster@puppet.com>
2020-09-14 16:56:47 -0700

    Added node.name parameter

--- manifests/install_elastic.pp
+++ manifests/install_elastic.pp
@@ -31,7 +31,8 @@ class puppet_logging_dashboard::install_elastic(
     config      => {
       'network.host'                        => '0.0.0.0',
       'http.port'                           => '9200',
-      'cluster.initial_master_nodes'        => $facts['fqdn'],
+      'node.name'                           => $facts['hostname'],
+      'cluster.initial_master_nodes'        => $facts['hostname'],
       'xpack.monitoring.collection.enabled' =>  true,
       'xpack.license.self_generated.type'   => 'basic',
     },
