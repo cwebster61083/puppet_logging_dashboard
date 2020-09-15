f707d364e5ac9eab8cac5e0d1360c70e5184aa98
Chris Webster <chirs.webster@puppet.com>
2020-09-14 17:00:35 -0700

    Updated elastic search version

--- manifests/install_elastic.pp
+++ manifests/install_elastic.pp
@@ -10,7 +10,7 @@ class puppet_logging_dashboard::install_elastic(
   include elastic_stack::repo
 
   class { 'elasticsearch':
-    version           => '7.8.0',
+    version           => '7.9.1',
     restart_on_change => true,
   }
 
