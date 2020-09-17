be322d86ed5dbcf681fa4f389d9ef38f3abdffb0
Chris Webster <chirs.webster@puppet.com>
2020-09-17 15:07:25 -0700

    Refactoring Module

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,9 +6,9 @@
 #   include puppet_logging_dashboard
 class puppet_logging_dashboard {
   include puppet_logging_dashboard::install_java
-  include puppet_logging_dashboard::install_elastic
-  include puppet_logging_dashboard::install_kibana
-  include puppet_logging_dashboard::install_logstash
+  # include puppet_logging_dashboard::install_elastic
+  # include puppet_logging_dashboard::install_kibana
+  # include puppet_logging_dashboard::install_logstash
 
   # Class['puppet_logging_dashboard::install_java']
   # -> Class['puppet_logging_dashboard::install_elastic']
--- manifests/install_java.pp
+++ manifests/install_java.pp
@@ -4,8 +4,10 @@
 #
 # @example
 #   include puppet_logging_dashboard::install_java
-class puppet_logging_dashboard::install_java {
-  package {'java-1.8.0-openjdk-devel':
-    ensure => present,
+class puppet_logging_dashboard::install_java(
+  String $java_package  = 'java-1.8.0-openjdk-devel',
+) {
+  class { 'java':
+    package => $java_package,
   }
 }
