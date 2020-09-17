1d82dfb456036b04aba742363f2e2f1a87257ef0
Chris Webster <chirs.webster@puppet.com>
2020-09-17 16:59:18 -0700

    asdf

--- manifests/install_java.pp
+++ manifests/install_java.pp
@@ -5,7 +5,7 @@
 # @example
 #   include puppet_logging_dashboard::install_java
 class puppet_logging_dashboard::install_java(
-  String $java_package  = undef,
+  String $java_package  = 'java-1.8.0-openjdk-devel',
 ) {
   class { 'java':
     package => $java_package,
