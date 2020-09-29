6fa82758d9bb70e4c27be916a5fa2a5e2c0bbab9
Chris Webster <chirs.webster@puppet.com>
2020-09-29 15:08:48 -0700

    asdf

--- manifests/configure_logstash.pp
+++ manifests/configure_logstash.pp
@@ -7,5 +7,6 @@
 class puppet_logging_dashboard::configure_logstash {
   file { '/etc/logstash/conf.d/puppetserver-log.conf':
     ensure => file,
+    source => 'puppet:///modules/puppet_logging_dashboard/puppetserver-log.conf',
   }
 }
