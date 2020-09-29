c2c59e28ccffd872e41f3934e5b8777d5289a640
Chris Webster <chirs.webster@puppet.com>
2020-09-29 14:53:32 -0700

    asdf

---
+++ manifests/configure_logstash.pp
@@ -0,0 +1,11 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include puppet_logging_dashboard::configure_logstash
+class puppet_logging_dashboard::configure_logstash {
+  file { '/etc/logstash/conf.d/puppetserver-log.conf':
+    ensure => file,
+  }
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -9,6 +9,7 @@ class puppet_logging_dashboard {
   include puppet_logging_dashboard::install_elastic
   include puppet_logging_dashboard::install_kibana
   include puppet_logging_dashboard::install_logstash
+  include puppet_logging_dashboard::configure_logstash
 
   Class['puppet_logging_dashboard::install_java']
   -> Class['puppet_logging_dashboard::install_elastic']
