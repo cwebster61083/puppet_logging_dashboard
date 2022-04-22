566ed7021cccc8cab625ea92fe06c8f4ac352000
Chris Webster <chris.webster@puppet.com>
2022-04-22 13:37:07 -0700

    asdf

--- manifests/install_elastic.pp
+++ manifests/install_elastic.pp
@@ -12,27 +12,27 @@ class puppet_logging_dashboard::install_elastic(
     restart_on_change => true,
   }
 
-  elasticsearch::instance { 'es-01':
-    jvm_options => [
-      '-Xms4g',
-      '-Xmx4g',
-      '#PrintGCDetails',
-      '#PrintGCDateStamps',
-      '#PrintTenuringDistribution',
-      '#PrintGCApplicationStoppedTime',
-      '#Xloggc',
-      '#UseGCLogFileRotation',
-      '#NumberOfGCLogFiles',
-      '#GCLogFileSize',
-      '#XX:UseConcMarkSweepGC',
-    ],
-    config      => {
-      'network.host'                        => '0.0.0.0',
-      'http.port'                           => '9200',
-      'node.name'                           => $facts['networking']['hostname'],
-      'cluster.initial_master_nodes'        => $facts['networking']['hostname'],
-      'xpack.monitoring.collection.enabled' =>  true,
-      'xpack.license.self_generated.type'   => 'basic',
-    },
-  }
+  # elasticsearch::instance { 'es-01':
+  #   jvm_options => [
+  #     '-Xms4g',
+  #     '-Xmx4g',
+  #     '#PrintGCDetails',
+  #     '#PrintGCDateStamps',
+  #     '#PrintTenuringDistribution',
+  #     '#PrintGCApplicationStoppedTime',
+  #     '#Xloggc',
+  #     '#UseGCLogFileRotation',
+  #     '#NumberOfGCLogFiles',
+  #     '#GCLogFileSize',
+  #     '#XX:UseConcMarkSweepGC',
+  #   ],
+  #   config      => {
+  #     'network.host'                        => '0.0.0.0',
+  #     'http.port'                           => '9200',
+  #     'node.name'                           => $facts['networking']['hostname'],
+  #     'cluster.initial_master_nodes'        => $facts['networking']['hostname'],
+  #     'xpack.monitoring.collection.enabled' =>  true,
+  #     'xpack.license.self_generated.type'   => 'basic',
+  #   },
+  # }
 }
