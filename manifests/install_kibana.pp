5ac2ac71466e9da15b89be689d05da780a3d4e93
Chris Webster <chris.webster@puppet.com>
2022-04-26 17:00:12 -0700

    asdf

--- manifests/install_kibana.pp
+++ manifests/install_kibana.pp
@@ -8,7 +8,7 @@ class puppet_logging_dashboard::install_kibana {
   class { 'kibana' :
     config => {
       'server.host'          => $facts['network'],
-      'server.publicBaseUrl' =>"http://$facts['network']:5601"
+      'server.publicBaseUrl' =>"http://$facts['network']:5601",
       'elasticsearch.hosts'  => 'http://localhost:9200',
       # 'xpack.license_management.enabled' => false,
       # 'elasticsearch.username'           => 'elastic',
