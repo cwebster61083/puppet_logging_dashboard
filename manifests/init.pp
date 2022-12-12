8d8ad78944d5450925d30b7bf3f905fbbc620651
Chris Webster <chris.webster@puppet.com>
2022-12-12 14:11:05 -0800

    asdf

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,7 +8,7 @@ class puppet_logging_dashboard {
   include puppet_logging_dashboard::install_java
   include puppet_logging_dashboard::install_elastic
   include puppet_logging_dashboard::install_kibana
-  # include puppet_logging_dashboard::install_logstash
+  include puppet_logging_dashboard::install_logstash
   # include puppet_logging_dashboard::configure_logstash
 
   Class['puppet_logging_dashboard::install_java']
